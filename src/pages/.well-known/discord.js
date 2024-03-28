b4e657915f3d476c172d1f4ffdc7b45ae0c64093
ShadowMonster99 <81448108+ShadowMonster99@users.noreply.github.com>
2024-03-27 23:14:33 -0300

    Update discord.js

--- src/pages/.well-known/discord.js
+++ src/pages/.well-known/discord.js
@@ -3,6 +3,7 @@ const Sitemap = () => null
 export const getServerSideProps = async ({ res }) => {
   if (res) {
     res.setHeader('Content-Type', 'text/plain')
+    // doesnt matter if its public
     res.write(`dh=5f233f3228adffea0707d6beadd925081b852641`)
     res.end()
   }
